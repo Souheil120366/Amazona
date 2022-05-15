1a154dcb56c6fd8d294a1846dc6b140f04c0a94f
Souheil <khalfallah.souheil@gmail.com>
2022-05-15 16:33:51 +0100

    15 05 2022 4:30 PM

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,21 @@
+import bcrypt from 'bcryptjs';
+
+
 const data = {
+    users: [
+        {
+          name: 'Souheil',
+          email: 'khalfallah.souheil@gmail.com',
+          password: bcrypt.hashSync('1234', 8),
+          isAdmin: true,
+        },
+        {
+          name: 'Iyadh',
+          email: 'user@example.com',
+          password: bcrypt.hashSync('1234', 8),
+          isAdmin: false,
+        },
+      ],
     products: [
         {
             _id:'1',
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import data from './data.js';
+import userRouter from './routers/userRouter.js';
 
 const app = express();
+
+mongoose.connect('mongodb://localhost/Amazona',{useNewUrlParser: true,useUnifiedTopology: true});
+
 
 app.get('/api/products/:id',(req,res) => {
     const product = data.products.find((x) => x._id === req.params.id);
@@ -16,11 +21,14 @@ app.get('/api/products/:id',(req,res) => {
 app.get('/api/products',(req,res) => {
     res.send(data.products);
 });
-
+app.use('/api/users', userRouter);
 app.get('/', (req, res) => {
     res.send('Server is ready');
 });
-
+app.use((err, req, res, next ) => {
+    res.status(500).send({ message: err.message });
+  });
+  
 app.listen(5000, () => {
     console.log('Serve at http://localhost:5000');
 });
