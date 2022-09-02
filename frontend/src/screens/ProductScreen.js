06ff937f6066e6e03ecd6d19715ee28d637cce20
Souheil <khalfallah.souheil@gmail.com>
2022-09-02 17:16:32 +0100

    02/09/2022 at 17h16

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -43,8 +43,8 @@ function ProductScreen() {
       dispatch({ type: 'FETCH_REQUEST' });
       try {
         
-        //const result = await axios.get(`http://www.skftechnologies.com:5000/api/products/slug/${slug}`);
-        const result = await axios.get(`/api/products/slug/${slug}`);
+        const result = await axios.get(`http://www.skftechnologies.com:5000/api/products/slug/${slug}`);
+        //const result = await axios.get(`/api/products/slug/${slug}`);
         dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
       } catch (err) {
         dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
